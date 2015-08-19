227613d0ce1670e17b6e2cae97ad28d7b4c0d4f1
Alex Bertrand <alex.bertrand13@gmail.com>
2015-08-19 12:02:23 -0500

    Begin edits for reading files during directory walk

--- DependencyVisualizer/routes/index.js
+++ DependencyVisualizer/routes/index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 exports.index = function(req, res){
   //Recursively walk the file tree.
   var path = './';
-  var fileData = 0;
+  var fileData = {};
   readFileTree(path);
 
   function readFileTree(path) {
@@ -17,12 +17,40 @@ exports.index = function(req, res){
         return;
       } else {
         files.map(function(file) {
-          if(fs.statSync(path + file).isFile()) {
-            //If we've hit a file, perform some action
-            console.log(path + file);
+          var newPath = path + file;
+          if(fs.statSync(newPath).isFile()) {
+            //If we've hit a file, read it for it's dependencies
+            console.log(newPath);
+            fs.readFile('newPath', function(error, data) {
+              if(error) {
+                console.log(error);
+              } else {
+                var fileData = data.toString();
+
+                var defineStatementRegex = new RegExp(/define\(\[\n(\s+\'[\w\/]+\',\n)+(\s+\'[\w\/]+\'\n)\],/g);
+                var defineStatement = defineStatementRegex.exec(fileData)[0]; //only get the matched string
+
+                var dependencyRegex = new RegExp(/\'([\w\/]+)\'/g);
+                var match, dependencies = [];
+
+                // Blacklist common library dependencies that don't really tell us anything.
+                var blacklist = ["ENV", "underscore", "jquery"];
+
+                while(match = dependencyRegex.exec(defineStatement)) {
+                  // take the capturing group (without the single quotes) instead of the whole match
+                  var exactMatch = match[1];
+                  if(blacklist.indexOf(exactMatch) == -1) {
+                    dependencies.push(exactMatch);
+                  }
+                }
+
+                fileData.newPath.dependencies = dependencies;
+                console.log(dependencies);
+              }
+            });
           } else {
             //If we've hit a directory, read through it
-            readFileTree(path + file + '/');
+            readFileTree(newPath + '/');
           }
         });
       }
