da15fab5d0f3a1c8f55202fe46dc061c11c58902
Alex Bertrand <alex.bertrand13@gmail.com>
2015-08-19 18:57:16 -0500

    Changed directory walk to be synchronous

--- DependencyVisualizer/routes/index.js
+++ DependencyVisualizer/routes/index.js
@@ -7,63 +7,51 @@ var fs = require('fs');
 exports.index = function(req, res){
   //Recursively walk the file tree.
   var path = './';
-  var fileData = {};
+  var fileDependencies = {};
   readFileTree(path);
 
   function readFileTree(path) {
-    fs.readdir(path, function(err, files) {
-      if(err) {
-        console.log(err);
-        return;
-      } else {
-        files.map(function(file) {
-          var newPath = path + file;
-          if(fs.statSync(newPath).isFile()) {
-            //If we've hit a file, read it for it's dependencies
-            console.log(newPath);
-            fs.readFile(newPath, function(error, data) {
-              if(error) {
-                console.log(error);
-              } else {
-                var fileData = data.toString();
+    var files = fs.readdirSync(path);
+    
+    //Process each item in the directory.  If it's a file, try to get it's dependencies.
+    //If it's a folder, process every item in it. 
+    files.map(function(file) {
+      var newPath = path + file;
+      if(fs.statSync(newPath).isFile()) {
+        //If we've hit a file, read it for it's dependencies
+        console.log(newPath);
+        var data = fs.readFileSync(newPath);
+        var fileData = data.toString();
 
-                var defineStatementRegex = new RegExp(/define\(\[\n(\s+\'[\w\/]+\',\n)+(\s+\'[\w\/]+\'\n)\],/g);
-                var defineStatement = defineStatementRegex.exec(fileData); //only get the matched string
-                if(!defineStatement) return;
-                defineStatement = defineStatement[0];
+        var defineStatementRegex = new RegExp(/define\(\[\n(\s+\'[\w\/]+\',\n)+(\s+\'[\w\/]+\'\n)\],/g);
+        var defineStatement = defineStatementRegex.exec(fileData);
+        if(!defineStatement) return;
+        defineStatement = defineStatement[0]; //only get the matched string
 
-                var dependencyRegex = new RegExp(/\'([\w\/]+)\'/g);
-                var match, dependencies = [];
+        var dependencyRegex = new RegExp(/\'([\w\/]+)\'/g);
+        var match, dependencies = [];
 
-                // Blacklist common library dependencies that don't really tell us anything.
-                var blacklist = ["ENV", "underscore", "jquery"];
+        // Blacklist common library dependencies that don't really tell us anything.
+        var blacklist = ["ENV", "underscore", "jquery"];
 
-                while(match = dependencyRegex.exec(defineStatement)) {
-                  // take the capturing group (without the single quotes) instead of the whole match
-                  var exactMatch = match[1];
-                  if(blacklist.indexOf(exactMatch) == -1) {
-                    dependencies.push(exactMatch);
-                  }
-                }
-
-                fileData.newPath = dependencies ? dependencies : null;
-                console.log(dependencies);
-              }
-            });
-            return;
-          } else {
-            //If we've hit a directory, read through it
-            readFileTree(newPath + '/');
+        while(match = dependencyRegex.exec(defineStatement)) {
+          // take the capturing group (without the single quotes) instead of the whole match
+          var exactMatch = match[1];
+          if(blacklist.indexOf(exactMatch) == -1) {
+              dependencies.push(exactMatch);
           }
-        });
+        }
+
+        fileDependencies[newPath] = {};
+        fileDependencies[newPath]["dependencies"] = dependencies;
+      } else {
+        //If we've hit a directory, read through it
+        readFileTree(newPath + '/');
       }
     });
   }
 
-  /*setTimeout(function() {
-    console.log("done parsing");
-  }, 0);*/
-
+  console.log(fileDependencies);
 
   res.render('index', { title: 'Express' });
 };
