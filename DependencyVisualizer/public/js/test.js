750d37f7bbe620ec816f4f022300548dc0770951
Alex Bertrand <alex.bertrand13@gmail.com>
2015-09-01 14:21:16 -0500

    Major code cleanup.
    Fixed the ordering of things so I'm only looping through everything twice, instead of like 5 times.  Should be working as it was before.  There was something else too, but I forgot it.

--- DependencyVisualizer/public/js/test.js
+++ DependencyVisualizer/public/js/test.js
@@ -1,59 +1,71 @@
 $(document).ready(function() {
 
   $.get("/map", function(data) {
+    //Set up canvas
     var canvas = Raphael(0,0,window.innerWidth, window.innerHeight);
     canvas.canvas.style.backgroundColor = "#2c3e50";
-
-    var dataKeys = Object.keys(data);
+    var allNodes = canvas.set();
     var selectedNode;
 
-    //Draw nodes
+    // Add proper attributes to nodes
+    // This will allow us to filter down to only the nodes that need to be shown
+    var dataKeys = Object.keys(data);
     dataKeys.map(function(el) {
-      var node = canvas.circle(Math.random() * canvas.width, Math.random() * canvas.height, 15);
+      if(!data[el].hasOwnProperty('pureDependent')) {
+        data[el].pureDependent = true;
+      }
+      var dependencies = data[el].dependencies;
+      //if the given item does indeed have dependencies
+      if(dependencies) {
+        data[el].used = true;
+        dependencies.map(function(dep) {
+          if(!data[dep]) { return; } // This fixes an issue with scoutfile modules not being found.  Need to look into fixing properly.
+          data[dep].used = true;
+          data[dep].pureDependent = false;
+          data[dep].score = data[dep].score ? ++(data[dep].score) : 1;
+          data[el].score = data[el].score ? --data[el].score : -1;
+        });
+      }
+    });
+    
+    //First loop - get nodes on teh page!
+    dataKeys.map(function(el) {
+      if(!data[el].used) { return; } //node doesn't need to be drawn
+      
+      //Generate a node!
+      var node = canvas.circle(0, 0, 10); //irrelevant starting values
       node.id = el;
-      node.attr("fill", colorHash(el));
-      node.attr("stroke", "none");
-      node.attr("stroke-width", 2); //fo lataz
-      node.score = 0;
-      node.used = false; //whether or not this node has either dependencies or dependents (TBD)
-      node.dependencies = data[el].dependencies;
+      node.score = data[el].score;
+      node.used = true;
+      node.pureDependent = data[el].pureDependent;
+      node.dependencies = data[el].dependencies || [];
       node.lines = [];
       node.operations = [];
-      node.pureDependent = true;
 
-      var nodeOp = 0; //keep track of node ops.
+      
+      //Apply teh prettiez.
+      styleNode(node);
+      setStartingPosition(node);
 
-      //add listeners for various important events
+      allNodes.push(node);
+
+
+      //Add listeners to make things do things
+      var nodeOp = 0;
+      
       node.dblclick(function() {
-        /*console.log("Name: " + node.data("file"));
-        console.log("Dependencies: " + node.data("dependencies"));
-        console.log("Score: " + node.data("score"));*/
-
-        /*walkUpTree(node, function(thisNode) {
-          thisNode.nodeGlow = thisNode.glow({
-            color : "#0FF",
-            width : 20,
-            opacity : 1
-          });
-        });*/
-
         if(selectedNode !== node) {
           //erase lines from the old node
           if(selectedNode) {
             nodeOp++;
             walkUpTree(selectedNode,
               function(node) {
-                //debugger;
-                if(node.lines) { //we need this check for dependencies that get called here, but not to create lines (because they themselves have no dependencies)
-                  //console.log(node.lines.length);
-                  node.lines.map(function(el) {
-                    //console.log("Removing a line");
-                    el.remove();
-                  });
-                  node.lines = [];
-                  node.undrag();
-                  node.drag(handleDrag, null, null, node);
-                }
+                node.lines.map(function(el) {
+                  el.remove();
+                });
+                node.lines = [];
+                node.undrag();
+                node.drag(handleDrag, null, null, node);
               },
               function() {}, "ERASELINES", true); //have it repeat for security.  Careful with cycles here.
           }
@@ -73,18 +85,13 @@ $(document).ready(function() {
         selectedNode = node;
       });
 
-      //USED TO BE MOUSEUP
-      node.mousedown(function() {
-        /*walkUpTree(node, function(thisNode) {
-          if(thisNode.nodeGlow) {
-            thisNode.nodeGlow.remove();
-            thisNode.nodeGlow = null;
-          }
-        });*/
-      });
-
       node.hover(hiliteNode, unHiliteNode, node, node);
 
+      node.drag(handleDrag, null, null, node);
+
+      node.click(function() {
+        console.log(node);
+      });
 
       function walkUpTree(node, nodeFn, nodeDepFn, operation, repeat) {
         repeat = repeat || false;
@@ -102,9 +109,34 @@ $(document).ready(function() {
           });
         }
       }
-
-      node.drag(handleDrag, null, null, node);
     });
+
+    function setStartingPosition(node) {
+      var box = 300;
+
+      if(node.score >= 5) {
+        node.attr("cx", canvas.width - Math.random() * box);
+        node.attr("cy", canvas.height - Math.random() * box);
+      }
+      else if(node.score == 1) {
+        node.attr("cx", 200 + Math.random() * (canvas.width - 200 - box));
+        node.attr("cy", canvas.height - 50);
+      }
+      else if(node.pureDependent) {
+        node.attr("cx", 400 + Math.random() * (canvas.width-500) );
+        node.attr("cy", 50);
+      } else {
+        node.attr("cy", 50 + Math.random() * (canvas.height-100));
+        node.attr("cx", 50 + 400 * Math.exp(-1 * node.attr("cy")/(canvas.height/4)));
+      }
+    }
+
+    function styleNode(node) {
+      node.attr("fill", colorHash(node.id));
+      node.attr("stroke", "none");
+      node.attr("stroke-width", 2);
+      node.attr("r", node.score <= 0 ? 10 : 15 * Math.log(node.score) + 10);
+    }
 
     function handleDrag(dx, dy, x, y) {
       this.attr("cx", x);
@@ -188,139 +220,6 @@ $(document).ready(function() {
         if(!dep) return;
         //draw the line
         var line = canvas.path();
-        
-        drawLine(line, node, dep);
-        styleLine(line);
-
-        node.drag(function() { drawLine(line, node, dep); });
-        dep.drag(function() { drawLine(line, node, dep); });
-
-        //mark the dependency as having a dependent.  Thus, should be on screen
-        dep.used = true;
-        dep.score++;
-      });
-    });*/
-    
-    // generate scores and whether a node is 'used' or not
-    dataKeys.map(function(el) {
-      var node = canvas.getById(el);
-      var nodeDeps = node.dependencies;
-      if(nodeDeps) {
-        node.used = true;
-        nodeDeps.map(function(dependency) {
-          var dep = canvas.getById(dependency);
-          if(dep) {
-            dep.used = true;
-            dep.pureDependent = false;
-            dep.score++;
-            node.score--;
-          }
-        });
-      }
-    });
-
-    // Prune all unused nodes
-    //figure out why bv/ugc/auth and scripts/scout are sticking around
-    dataKeys.map(function(el) {
-      var node = canvas.getById(el);
-      if(!node.used) {
-        node.remove();
-      }
-    });
-
-    // Realizing now that we're taking up processing power going through nodes that aren't on screen.  WE'RE BURNING MOONLIGHT
-    // possibly make a set of all nodes remaining after pruning (or do this during pruning) and then only work with those
-    // NOTE TO SELF MAKE A SEARCH FEATURE OTHERWISE THIS IS USELESS
-
-    //move nodes to places on screen that (might?!) make sense
-    dataKeys.map(function(el) {
-      var node = canvas.getById(el);
-      var box = 300;
-      //size nodes interestingly
-      if(node) {
-        //node.attr("r", node.score > 5 ? node.score : 5);
-        node.attr("r", node.score <= 0 ? 10 : 15 * Math.log(node.score) + 10);
-      }
-      
-      // Dependencies on bottom right, dependents on top left.
-      /*if(node && node.score > 0) {
-        node.attr("cx", Math.random() * box + canvas.width - box);
-        node.attr("cy", Math.random() * box + canvas.height - box);
-      }*/
-      /*if(node && node.score < 0) {
-        node.attr("cx", Math.random() * box);
-        node.attr("cy", Math.random() * box);
-      }*/
-
-      //Heavy dependencies in center
-      /*if(node && node.score > 10) {
-        node.attr("cx", canvas.width/2 + (Math.random() * box) - box/2);
-        node.attr("cy", canvas.height/2 + (Math.random() * box) - box/2);
-      }
-      if(node && node.score < 0) {
-        node.attr("cx", Math.random() * canvas.width);
-        node.attr("cy", 20);
-      }
-      if(node && node.score > 0 && node.score < 10) {
-        node.attr("cx", Math.random() * canvas.width);
-        node.attr("cy", canvas.height - 50);
-      }*/
-
-      //Dependents along center axis
-      /*if(node && node.pureDependent) {
-        var p = Math.random() * .9 + .05;
-        node.attr("cx", p * canvas.width);
-        node.attr("cy", p * canvas.height);
-      }
-      else if(node && node.score > 10) {
-        if(Math.random() > .5) {
-          node.attr("cx", Math.random() * box);
-          node.attr("cy", canvas.height - Math.random() * box);
-        } else {
-          node.attr("cx", canvas.width - Math.random() * box);
-          node.attr("cy", Math.random() * box);
-        }
-      }
-      else if(node && node.score == 1) {
-        node.attr("cx", 1.5*box + Math.random() * (canvas.width - box*3));
-        node.attr("cy", canvas.height - 50);
-      } else if(node) {
-        node.attr("cx", Math.random() * box)
-        node.attr("cy", Math.random() * box + box/2);
-      }*/
-
-      //Herp derp.
-      if(node && node.score > 5) {
-        node.attr("cx", canvas.width - Math.random() * box);
-        node.attr("cy", canvas.height - Math.random() * box);
-      }
-      else if(node && node.score == 1) {
-        node.attr("cx", 200 + Math.random() * (canvas.width - 200 - box));
-        node.attr("cy", canvas.height - 50);
-      }
-      else if(node && node.pureDependent) {
-        node.attr("cx", 400 + Math.random() * (canvas.width-500) );
-        node.attr("cy", 50);
-      } else if(node) {
-        node.attr("cx", 50 + 400 * Math.exp(-1 * node.attr("cy")/(canvas.height/4)));
-      }
-    });
-
-    //Draw lines between nodes
-    //There's probably some fancy algorithm to optimize this, but I'm probably not going to use it.
-    //UNCOMMENT TO SEE SHITSHOW
-    /*dataKeys.map(function(el) {
-      var node = canvas.getById(el);
-      if(!node) { return; }
-      var nodeDeps = node.dependencies;
-      if(!nodeDeps) { return; }
-      node.used = true; //if it has dependencies, we want to show it on screen
-      nodeDeps.map(function(dependency) {
-        var dep = canvas.getById(dependency);
-        //hacky, fix eventually
-        if(!dep) return;
-        //draw the line
-        var line = canvas.path();
         drawLine(line, node, dep);
         styleLine(line);
 
@@ -336,15 +235,11 @@ $(document).ready(function() {
     //set up node search
     $('#searchbar').keyup(function() {
       var text = $(this).val();
-      dataKeys.map(function(el) {
-        if(el.indexOf(text) != -1 && text.length > 0) {
-          var node = canvas.getById(el);
-          if(!node) { return; }
+      allNodes.forEach(function(node) {
+        if(node.id.indexOf(text) != -1 && text.length > 0) {
           hiliteNode.call(node);
         }
-        else {
-          var node = canvas.getById(el);
-          if(!node) { return; }
+        else {  
           unHiliteNode.call(node);
         }
       });
