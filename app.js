04d69e2df44a600523ebd73ae327026ab58a4732
Alex Bertrand <alex.bertrand13@gmail.com>
2015-09-04 08:58:46 -0500

    Minor code cleanup

--- app.js
+++ app.js
@@ -2,26 +2,20 @@ var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-//var methodOverride = require('method-override');
-//var errorHandler = require('error-handler');
 
 var routes = require('./routes/index');
 
 var app = express();
 
-// all environments
-//app.set('port', process.env.PORT || 4539);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//app.use(methodOverride);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-//app.use('/map', routes.map);
 
 
 // catch 404 and forward to error handler
@@ -30,12 +24,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-
-// development only
-//if ('development' == app.get('env')) {
-  //app.use(errorHandler);
-//}
 
 // error handlers
 
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,10 @@ var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
-/*
- * GET home page.
- */
 
 router.get('/', function(req, res, next) {
-//exports.index = function(err, req, res, next) {
   console.log('Hello World!');
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dependency Visualizer' });
 });
 
 router.get('/map',  function(req, res, next) {
